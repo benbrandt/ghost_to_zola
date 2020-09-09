d6289c4d33bcc1dde2ddf77022d5f33bcd73f281
Ben Brandt <benjamin.j.brandt@gmail.com>
2020-09-08 21:12:35 -0700

    Initial clap setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use clap::clap_app;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let matches = clap_app!(myapp =>
+        (version: "1.0")
+        (author: "Ben Brandt <benjamin.j.brandt@gmail.com>")
+        (about: "Migrates a Ghost export file to Zola markdown content format.")
+        (@arg INPUT: +required "Sets the input file to use")
+    )
+    .get_matches();
+
+    let file = matches.value_of("INPUT").unwrap();
+    println!("The file passed is: {}", file);
+
+    Ok(())
 }
