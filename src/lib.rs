027561d96bde12b3206eafb4b2cacd698a0f6a73
Ben Brandt <benjamin.j.brandt@gmail.com>
2020-09-12 21:47:16 -0700

    Now that's more like it!

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,41 @@
 pub mod ghost {
     use chrono::{DateTime, Utc};
-    use serde::Deserialize;
+    use serde::{Deserialize, Deserializer};
     use std::error::Error;
     use std::fs::File;
     use std::io::BufReader;
     use std::path::Path;
+    #[derive(Debug, Deserialize)]
+    pub struct Card {
+        #[serde(default)]
+        pub markdown: String,
+    }
+
+    #[derive(Debug, Deserialize)]
+    pub struct MobileDoc {
+        pub cards: Vec<(String, Card)>,
+    }
 
     #[derive(Debug, Deserialize)]
     pub struct Post {
         pub title: String,
         pub slug: String,
-        pub mobiledoc: String,
+        #[serde(deserialize_with = "mobile_doc")]
+        pub mobiledoc: MobileDoc,
         pub comment_id: String,
         pub feature_image: Option<String>,
         pub created_at: DateTime<Utc>,
+        pub updated_at: DateTime<Utc>,
         pub published_at: Option<String>,
         pub r#type: String,
+    }
+
+    fn mobile_doc<'de, D>(deserializer: D) -> Result<MobileDoc, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        serde_json::from_str(&s).map_err(serde::de::Error::custom)
     }
 
     #[derive(Debug, Deserialize)]
@@ -45,6 +65,69 @@ pub mod ghost {
             Ok(data.posts)
         } else {
             Ok(vec![])
+        }
+    }
+}
+
+pub mod zola {
+    use crate::ghost;
+    use chrono::{DateTime, Utc};
+    use std::fmt;
+
+    pub struct Page {
+        /// The markdown content of the post
+        content: String,
+        /// The date of the page
+        date: DateTime<Utc>,
+        /// Whether the page is published yet or not
+        draft: bool,
+        /// Slug for the url of the page
+        slug: String,
+        /// Title of the page
+        title: String,
+        /// The last updated date of the post
+        updated: DateTime<Utc>,
+        // TODO: taxonomies: Vec<String>,
+    }
+
+    impl From<ghost::Post> for Page {
+        fn from(
+            ghost::Post {
+                created_at,
+                mobiledoc,
+                slug,
+                published_at,
+                title,
+                updated_at,
+                ..
+            }: ghost::Post,
+        ) -> Self {
+            let content = mobiledoc
+                .cards
+                .iter()
+                .fold(String::new(), |mut acc, (_, c)| {
+                    acc.push_str(&c.markdown);
+                    acc
+                });
+            Self {
+                content,
+                date: created_at,
+                draft: published_at.is_none(),
+                slug,
+                title,
+                updated: updated_at,
+            }
+        }
+    }
+
+    impl fmt::Display for Page {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            // Frontmatter
+            writeln!(f, "+++")?;
+            writeln!(f, "title = \"{}\"", self.title)?;
+            writeln!(f, "+++")?;
+            // Body
+            writeln!(f, "{}", self.content)
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::clap_app;
-use ghost_to_zola::ghost;
+use ghost_to_zola::{ghost, zola};
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -14,7 +14,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let path = matches.value_of("INPUT").unwrap();
 
     let posts = ghost::read_posts_from_file(path)?;
-    println!("Post Title: {}", posts.first().unwrap().created_at);
+    for post in posts {
+        println!("{}", zola::Page::from(post));
+    }
 
     Ok(())
 }
